6f4b5608411e4ee338a7abaee549ce7c9fffe9c2
Vikour <vikour92@gmail.com>
2017-11-28 00:09:00 +0100

    Importado completado

--- src/modelo/Importador.java
+++ src/modelo/Importador.java
@@ -6,9 +6,15 @@
 package modelo;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.zip.*;
 
 /**
  *
@@ -19,41 +25,87 @@ public class Importador {
     private static final String EXTENSION_COMPRIMIDO = ".zip";
     private FormatoFichero fmt;
     private File file;
+    private File tmp;
 
-    public Importador(FormatoFichero fmt, File file) throws FileNotFoundException {
+    public Importador(FormatoFichero fmt, File file) {
         this.fmt = fmt;
         this.file = file;
-        
-        if (!file.exists())
-            throw new FileNotFoundException();
-        
+        this.tmp = null;
     }
     
-    public List<Object> importar() {
+    public List<Object> importar() throws FileNotFoundException, IOException {
         List<Object> list = new ArrayList<>();
         List<File> files = new ArrayList<>();
-        
-        if (!file.isDirectory() && file.getName().matches("[a-zA-Z0-9]+."+EXTENSION_COMPRIMIDO))
-            descomprimir();
-        
-        if (file.isDirectory())
-            files = leerFicheros();
-        else
-            files.add(file);
-        
-        for (File f : files) {
-            Object o = fmt.leer(f);
-            
-            if (o != null)
-                list.add(o);
-            
+
+        try {
+            if (!file.isDirectory() && file.getName().matches("[a-zA-Z0-9]+" + EXTENSION_COMPRIMIDO)) {
+                files = descomprimir();
+            } else if (file.isDirectory()) {
+                files = leerFicheros();
+            } else {
+                files.add(file);
+            }
+
+            for (File f : files) {
+                Object o = fmt.leer(f);
+
+                if (o != null) {
+                    list.add(o);
+                }
+
+            }
+        } finally {
+
+            if (tmp != null) {
+                borrarFicherosTemporales(tmp);
+            }
+
         }
-        
+
         return list;
     }
     
-    private void descomprimir() {
-        System.out.println("Descomprimir");
+    private List<File> descomprimir() throws FileNotFoundException, IOException {
+        ZipInputStream zis = null;
+        List<File> files = new ArrayList<>();
+        
+        tmp = File.createTempFile(file.getName().replace(EXTENSION_COMPRIMIDO, ""), 
+                                    Long.toString(System.nanoTime()));
+        
+        if (tmp.exists())
+            tmp.delete();
+        
+        if (!(tmp.mkdir()))
+            throw new IOException("No se pudo descomprimir el fichero " + file.getName());
+        
+        
+        try {
+            zis = new ZipInputStream(new FileInputStream(file));
+            ZipEntry entry = null;
+            
+            while ((entry  = zis.getNextEntry()) != null) {
+                File f = new File(tmp.getAbsolutePath() + File.separator + entry.getName());
+
+                if (entry.isDirectory())
+                    f.mkdir();
+                else {
+                    f.createNewFile();
+                    files.add(f);
+                    descomprimirFichero(zis, f);
+                }
+
+            }
+            
+        }
+        finally {
+            // borrar los residuos si !success
+            
+            if (zis != null)
+                zis.close();
+            
+        }
+        
+        return files;
     }
     
     private List<File> leerFicheros() {
@@ -63,6 +115,40 @@ public class Importador {
             files.add(f);
         
         return files;
+    }
+
+    private void descomprimirFichero(ZipInputStream zis, File f) throws FileNotFoundException, IOException {
+        byte[] buffer = new byte[1024];
+        int leido;
+        FileOutputStream fos = null;
+
+        try {
+            fos = new FileOutputStream(f);
+            
+            while (0 < (leido = zis.read(buffer))) {
+                fos.write(buffer, 0, leido);
+            }
+
+            fos.close();
+        }
+        finally {
+            if (fos != null)
+                fos.close();
+        }
+
+    }
+
+    private void borrarFicherosTemporales(File tmp) {
+        
+        if (tmp.isFile()) 
+            tmp.delete();
+        else {
+            
+            for (File f : tmp.listFiles())
+                borrarFicherosTemporales(f);
+
+            tmp.delete();
+        }
     }
     
 }
