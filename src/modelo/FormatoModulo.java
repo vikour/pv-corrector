daa59b6c07c24ad20fee15e4eb2d44d9555cd1b2
MagicElyas <elias2603@hotmail.com>
2017-11-28 09:51:32 +0100

    Implementacion parcial de modulo
    añado campos a modulo

--- src/modelo/FormatoModulo.java
+++ src/modelo/FormatoModulo.java
@@ -6,6 +6,12 @@
 package modelo;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,7 +25,20 @@ public class FormatoModulo extends FormatoFichero {
 
     @Override
     public Object leer(File file) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            Scanner sc =new Scanner(file);
+            sc.useDelimiter("\\s*[\n]\\s*");
+
+            
+            
+            
+            
+            
+            
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(FormatoModulo.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
 
     @Override
