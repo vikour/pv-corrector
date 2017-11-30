1929e1ccd0d45e645375565816a487286e94cd00
Vikour <vikour92@gmail.com>
2017-11-30 20:38:24 +0100

    Formato Campaña casi terminado. Camapaña refractorizada.

--- src/modelo/Campaña.java
+++ src/modelo/Campaña.java
@@ -1,54 +1,55 @@
 /**
  * @(#) Campaña.java
  */
-
 package modelo;
 
 import java.util.List;
 
-public class Campaña
-{
+public class Campaña {
 
     static List<Campaña> listar(Modulo aThis) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
-   private Modulo modulo;
-   private String nombre;
-   private List<CurvaMedida> curvas;
-   private List<Canal> canales;
-   
-   /**
-    * @param modulo  Referencia al modulo que pertenece la nueva campaña
-    * @param nombre  Nombre de la nueva campaña.
-     * @param store  Si store = true, signfica que es de insercción, false es de consulta.
-    */
-   public Campaña(Modulo modulo, String nombre, boolean store) throws RuntimeException {
-       String insert = "INSERT INTO campanyas VALUES ('"+nombre+"','"+modulo.getNombre()+"')";
-       String select = "SELECT * FROM campanyas WHERE nombre = '" + nombre + "' AND " +
-                       "modulo = '" + modulo.getNombre() + "';";
-       BD bd = BD.getInstance();
-       
-       if (store) 
-           bd.insert(insert);
-       else if (bd.select(select).isEmpty())
-           throw new RuntimeException("No existe la campaña con módulo = " + modulo.getNombre() + " y nomre = " + nombre);
-       
-       // No hago mas con el select, porque no tiene por ahora más propiedades campaña.
-       this.modulo = modulo;
-       this.nombre = nombre;
-       curvas = null; // Lazy links.
-       canales = null;
-   }
+    
+    private Modulo modulo;
+    private String nombre;
+    private List<CurvaMedida> curvas;
+    private List<Canal> canales;
+
+    /**
+     * @param modulo Referencia al modulo que pertenece la nueva campaña
+     * @param nombre Nombre de la nueva campaña.
+     * @param store Si store = true, signfica que es de insercción, false es de
+     * consulta.
+     */
+    public Campaña(Modulo modulo, String nombre, boolean store) throws RuntimeException {
+        String insert = "INSERT INTO campanyas VALUES ('" + nombre + "','" + modulo.getNombre() + "')";
+        String select = "SELECT * FROM campanyas WHERE nombre = '" + nombre + "' AND "
+                + "modulo = '" + modulo.getNombre() + "';";
+        BD bd = BD.getInstance();
+
+        if (store) {
+            bd.insert(insert);
+        } else if (bd.select(select).isEmpty()) {
+            throw new Error("No existe la campaña con módulo = " + modulo.getNombre() + " y nomre = " + nombre);
+        }
+
+        // No hago mas con el select, porque no tiene por ahora más propiedades campaña.
+        this.modulo = modulo;
+        this.nombre = nombre;
+        curvas = null; // Lazy links.
+        canales = null;
+    }
 
     public Modulo getModulo() {
         return modulo;
     }
 
     public void setModulo(Modulo modulo) {
-        String update = "UPDATE campanyas SET modulo = '" + modulo.getNombre() + "' " +
-                        "WHERE nombre = '" + this.nombre + "' AND " +
-                        " modulo = '" + this.modulo.getNombre() + "'";
-        
+        String update = "UPDATE campanyas SET modulo = '" + modulo.getNombre() + "' "
+                + "WHERE nombre = '" + this.nombre + "' AND "
+                + " modulo = '" + this.modulo.getNombre() + "'";
+
         if (!modulo.equals(this.modulo)) {
             BD.getInstance().update(update);
             this.modulo = modulo;
@@ -60,10 +61,10 @@ public class Campaña
     }
 
     public void setNombre(String nombre) {
-        String update = "UPDATE campanyas SET nombre = '" + nombre + "' " +
-                        "WHERE nombre = '" + this.nombre + "' AND " +
-                        " modulo = '" + modulo.getNombre() + "'";
-        
+        String update = "UPDATE campanyas SET nombre = '" + nombre + "' "
+                + "WHERE nombre = '" + this.nombre + "' AND "
+                + " modulo = '" + modulo.getNombre() + "'";
+
         if (!this.nombre.equals(nombre)) {
             BD.getInstance().update(update);
             this.nombre = nombre;
@@ -71,18 +72,20 @@ public class Campaña
     }
 
     public List<CurvaMedida> getCurvas() {
-        
-        if (curvas == null)
-            curvas  = CurvaMedida.listar(this);
-        
+
+        if (curvas == null) {
+            curvas = CurvaMedida.listar(this);
+        }
+
         return curvas;
     }
 
     public List<Canal> getCanales() {
-        
-        if (canales == null)
+
+        if (canales == null) {
             canales = Canal.listar(this);
-        
+        }
+
         return canales;
     }
 
@@ -93,9 +96,9 @@ public class Campaña
 
     @Override
     public boolean equals(Object obj) {
-        return (obj instanceof Campaña) &&
-               (((Campaña) obj).modulo.equals(modulo)) &&
-               (((Campaña) obj).nombre.equals(nombre));
+        return (obj instanceof Campaña)
+                && (((Campaña) obj).modulo.equals(modulo))
+                && (((Campaña) obj).nombre.equals(nombre));
     }
-    
+
 }
--- src/modelo/FormatoCampaña.java
+++ src/modelo/FormatoCampaña.java
@@ -5,7 +5,13 @@
  */
 package modelo;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 public class FormatoCampaña extends FormatoFichero{
 
@@ -15,7 +21,47 @@ public class FormatoCampaña extends FormatoFichero{
 
     @Override
     public Object leer(File file) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        BufferedReader br = null;
+        BD bd = BD.getInstance();
+        Campaña campaña = null;
+        
+        
+        try {
+            bd.execute("BEGIN TRANSACTION");
+            br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "ISO-8859-15"));
+            
+            campaña = leerInfoBasica(br);
+            leerInfoBasicaCurva(br, campaña);
+            leerCanales(br);
+            leerMedidasCurva(br);
+        
+            bd.execute("COMMIT");
+        } 
+        catch (PatternSyntaxException | NumberFormatException ex) {
+            bd.execute("ROLLBACK");
+            notificar.alertFormatoFichero("Formato del fichero incorrecto");
+        }
+        catch (IOException ex) {
+            bd.execute("ROLLBACK");
+            ex.printStackTrace();
+        }
+        
+        return campaña;
+    }
+
+    private String readNotEmptyLine(BufferedReader br) throws IOException {
+        String aux = null;
+        
+        do {
+            aux = br.readLine();
+        } while (aux.isEmpty());
+        
+        return aux;
+    }
+
+    private String extractValue(String line) {
+        line = line.trim();
+        return line.split("\t")[1];
     }
 
     @Override
@@ -25,7 +71,91 @@ public class FormatoCampaña extends FormatoFichero{
 
     @Override
     public String getExtension() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return "xls";
     }
-    
+
+    private void leerCanales(BufferedReader br) throws IOException {
+        String line;
+        String parts[];
+        
+        String magnitud, nombreCanal, valorStr;
+        double valor;
+        
+        line = readNotEmptyLine(br);
+        
+        while (!line.contains("NÃºmero de puntos curva IV")) {
+            parts = line.split(":");
+            nombreCanal = parts[parts.length-2];
+            parts = parts[parts.length-1].split("\t");
+            valorStr = parts[parts.length-2].replace(",", ".");
+            
+            if (!valorStr.isEmpty()) {
+                valor = Double.valueOf(valorStr);
+                magnitud = parts[parts.length-1];
+                // Insertar canal.
+            }
+            
+            line = readNotEmptyLine(br);            
+        }
+ 
+    }
+
+    private void leerMedidasCurva(BufferedReader br) throws IOException {
+        String line;
+        int numero;
+        String intensidadStr;
+        String tensionStr;
+        String parts[];
+        
+        line = readNotEmptyLine(br); //saltamos la cabecera.
+        line = readNotEmptyLine(br);        
+        
+        while (line != null) {
+            parts = line.split("\t");
+            numero = Integer.valueOf(parts[0]);
+            tensionStr = parts[1].replace(",", ".");
+            intensidadStr = parts[2].replace(",", ".");
+            line = readNotEmptyLine(br);
+        }
+        
+    }
+
+    private void leerInfoBasicaCurva(BufferedReader br, Campaña campaña) throws IOException {         
+        String line;
+
+        // Por hacer, leer fechas.
+        line = readNotEmptyLine(br);
+        line = readNotEmptyLine(br);
+        // fin leer fechas.
+        line = readNotEmptyLine(br); // Ignoramos correcciÃ³n.
+        
+        for (int i = 0 ; i < 6 ; i++)
+            line = readNotEmptyLine(br);
+        
+    }
+
+    private Campaña leerInfoBasica(BufferedReader br) throws IOException {
+        String line;
+        Modulo modulo = null;
+        Campaña Campaña = null;
+
+        line = readNotEmptyLine(br);
+
+        try { // Modulo.
+            modulo = new Modulo(extractValue(line));
+        } catch (Error ex) {
+            modulo = new Modulo(extractValue(line),"");
+        }
+
+        try { // CampaÃ±a.
+            line = readNotEmptyLine(br);
+            Campaña = new Campaña(modulo, extractValue(line), false);
+        } catch (Error err) {
+            Campaña = new Campaña(modulo, extractValue(line), true);
+        } // Si existe la campaÃ±a no hacemos nada.    
+
+        return Campaña;
+    }
+
 }
+
