84ec0e3ecec275ccabfd8cc8c51bed9be3bf5442
EzequielRodriguez <ezequiel.rm.97gmail.com>
2017-12-02 17:12:36 +0100

    Requisito 3.1 En proceso

---
+++ src/controlador/CtrAdminMedidas.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controlador;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ *
+ * @author EzequielRodriguez
+ */
+public class CtrAdminMedidas implements ActionListener{
+
+  /*  
+    public CtrAdminMedidas(ViewAdminMedidas vm) {
+        this.vm = vm;
+        vm.setControlador(this);
+        ctrs=null;
+       
+       
+    }
+*/
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+}
